a4cddf6c8a0b343755fc83900bba15a328200cb8
maisharahman876 <maisharahman876@gmail.com>
2021-12-01 23:31:58 +0600

    file distinguish

--- Offline_SS/bash.sh
+++ Offline_SS/bash.sh
@@ -3,12 +3,34 @@ all_file(){
 for path in "$1"/*; do
 	if [ -d "$path" ]
 	then
-		for dfile in "$path"/*; do
+		for dfile in "$path"/; do
 			all_file "$dfile"
 		done
 	else
-		echo "$path"
+		files+=($path)
 	fi
 	done
 }
-all_file "$1"
+arr=()
+files=()
+i=0
+read -p "Enter Directory: " dir
+read -p "Enter Source File: " file
+while read line;
+do 
+	arr+=($line)
+	#echo $line
+done < $file
+all_file "$dir"
+for i in ${!files[@]}; do
+	path="${files[$i]}"
+	filename=$(basename -- "$path")
+	fileext=${filename##*.}
+	if [[ "$filename" != "$fileext" ]]
+	then
+		
+		echo "$fileext" 
+		echo "$filename"
+
+	fi
+done
