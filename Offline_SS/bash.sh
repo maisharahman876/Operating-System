24e2e40760c7d6a8ec38f26065c3d8ec9ecc7ecd
maisharahman876 <maisharahman876@gmail.com>
2021-12-02 15:27:32 +0600

    copleted

--- Offline_SS/bash.sh
+++ Offline_SS/bash.sh
@@ -8,16 +8,20 @@ for path in "$1"/*; do
 		done
 	else
 		files+=($path)
+
 	fi
 	done
 }
 arr=()
 files=()
 i=0
+ignored=0
 read -p "Enter Directory: " dir
 read -p "Enter Source File: " file
 curr_dir="$(pwd)"/
 mkdir -p output_dir
+touch output.csv
+echo file_type,no_of_files>> output.csv
 while read line;
 do 
 	arr+=($line)
@@ -35,16 +39,22 @@ for i in ${!files[@]}; do
 			ex="${arr[$k]}"
 			if [[ "$ex" = "$fileext" ]];
 			then
-				j='expr $j+1'
+				j=$((j+1))
 			fi
 		done
 
 		if [[ "$j" = 0 ]];
 		then
+			if [[ -v "$fileext" ]]; then
+				$fileext=0
+			fi
 			mkdir -p output_dir/"$fileext"
 			cp "$path" output_dir/"$fileext"
 			touch -a output_dir/"$fileext"/desc_"$fileext".txt
 			echo "$path" >>output_dir/"$fileext"/desc_"$fileext".txt
+			
+		else
+			ignored=$((ignored+1))
 		fi
 	else
 		mkdir -p output_dir/others
@@ -53,3 +63,14 @@ for i in ${!files[@]}; do
 		echo "$path" >>output_dir/others/desc_others.txt
 	fi
 done
+for i in output_dir/*; do
+	size=-1
+	for j in "$i"/*; do
+		size=$((size+1))
+	done
+	filename=$(basename -- "$i")
+
+	echo $filename,"$size" >> output.csv
+
+done
+echo ignored,$ignored >> output.csv
