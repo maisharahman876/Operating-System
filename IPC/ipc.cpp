5a40183d06d77d3a74de6b27b0b8349e8ed0c661
maisharahman876 <maisharahman876@gmail.com>
2022-01-02 23:42:34 +0600

    upto kiosk

--- IPC/ipc.cpp
+++ IPC/ipc.cpp
@@ -7,6 +7,9 @@
 using namespace std;
 
 int n,kiosks,belts,per_belt,kiosk_time,security_time,boarding_time,vip_time;
+int kiosk_count=1;
+//sem_t k;
+pthread_mutex_t *mtx_k;
 
 std::default_random_engine generator;
 
@@ -65,10 +68,37 @@ void * airport(void * arg)
 	 int i=param[0];
 	 passenger* p=passengers[i];
 	 time_t end=time(&end);
+	 string s="";
 	 if(p->is_vip())
-	 cout<<"Passenger "<<i+1<<"(VIP) has arrived at the airport at time "<<end-start+1<<endl;
-	else
-		cout<<"Passenger "<<i+1<<" has arrived at the airport at time "<<end-start+1<<endl;
+	 	s="(VIP)";
+
+	 
+	 cout<<"Passenger "<<i+1<<s<<" has arrived at the airport at time "<<end-start+1<<endl;
+	
+	//Kiosk enter
+
+
+	int j=rand()%kiosks+1;
+	
+	pthread_mutex_lock(&mtx_k[j-1]);
+	//sem_getvalue(&k,&j);
+	end=time(&end);
+	
+	 cout<<"Passenger "<<i+1<<s<<" has started self-check in at kiosk "<<j<<" at time "<<end-start+1<<endl;
+	
+	
+	sleep(kiosk_time);
+
+	 end=time(&end);
+	 
+	cout<<"Passenger "<<i+1<<s<<" has finished check in at kiosk "<<j<<" at time "<<end-start+1<<endl;
+	pthread_mutex_unlock(&mtx_k[j-1]);
+	
+	//pthread_mutex_unlock(&mtx);
+	//sem_post(&k);
+
+
+
 }
 int main(int argc,char *argv[]){
 
@@ -77,6 +107,12 @@ int main(int argc,char *argv[]){
   	file.open(argv[1]);
 	file>>kiosks>>belts>>per_belt;
 	file>>kiosk_time>>security_time>>boarding_time>>vip_time;
+
+	mtx_k=new pthread_mutex_t[kiosks];
+	//sem_init(&k,0,kiosks);
+	for(int m=0;m<kiosks;m++)
+		pthread_mutex_init(&mtx_k[m],NULL);
+
 	//cout<<kiosks<<endl<<belts<<endl<<per_belt;
 	passengers=new passenger*[n];
 	//n=100;
