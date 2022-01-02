36fc2ce7e868dde83f77c1bc2c93292b6c8cbe6e
maisharahman876 <maisharahman876@gmail.com>
2022-01-02 20:37:34 +0600

    ghg

--- IPC/ipc.cpp
+++ IPC/ipc.cpp
@@ -11,6 +11,7 @@ int n,kiosks,belts,per_belt,kiosk_time,security_time,boarding_time,vip_time;
 std::default_random_engine generator;
 
 std::poisson_distribution<int> distribution(3);
+
 time_t start=time(&start);
 
 class passenger
@@ -55,8 +56,20 @@ bool passenger::lost()
 {
 	return lost_ticket;
 }
-std::vector<passenger*> passengers;
 
+passenger** passengers;
+
+void * airport(void * arg)
+{
+	 int *param = (int *)arg;
+	 int i=param[0];
+	 passenger* p=passengers[i];
+	 time_t end=time(&end);
+	 if(p->is_vip())
+	 cout<<"Passenger "<<i+1<<"(VIP) has arrived at the airport at time "<<end-start+1<<endl;
+	else
+		cout<<"Passenger "<<i+1<<" has arrived at the airport at time "<<end-start+1<<endl;
+}
 int main(int argc,char *argv[]){
 
 	srand(time(0));
@@ -65,15 +78,26 @@ int main(int argc,char *argv[]){
 	file>>kiosks>>belts>>per_belt;
 	file>>kiosk_time>>security_time>>boarding_time>>vip_time;
 	//cout<<kiosks<<endl<<belts<<endl<<per_belt;
+	passengers=new passenger*[n];
 	//n=100;
 	int i=0;
+	n=1000;
 	start=time(&start);
+	//char* mi="meo";
+	
 	while(1)
 	{
+		int r=rand()%n;
 		passenger* p=new passenger(i);
-		passengers.push_back(p);
+		passengers[i%n]=p;
+		if(i%5==4||i%7==2||i%9==5||i%11==10)
+			p->make_vip();
 		time_t end=time(&end);
-		cout<<"generated passenger "<<i<<" at "<<end-start<<endl;
+		int *param = (int *)malloc(1 * sizeof(int));
+		param[0] = i;
+		//cout<<"generated passenger "<<i<<" at "<<end-start<<endl;
+		pthread_t thread;
+		pthread_create(&thread,NULL,airport,param );
 		int sleeptime=distribution(generator);
 		sleep(sleeptime);
 		i++;
